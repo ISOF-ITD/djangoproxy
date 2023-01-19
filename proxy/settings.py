d854b00db5ed5f0596979e11c4dfa0964cde6f2b
Rico Simke <rico.simke@isof.se>
2023-01-19 20:04:16 +0100

    secret_key in secrets_env

--- proxy/settings.py
+++ proxy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import secrets_env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-38%d_+sarj*5(^s)ww9m#dkexfckmf3r%%*1^-bx-f%%555iji'
+SECRET_KEY = secrets_env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
